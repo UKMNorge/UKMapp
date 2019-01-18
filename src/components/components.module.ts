0bc5006caf4cc6694c6f0b807a992cff6bfb6dfe
Marius Mandal <mariusmandal@gmail.com>
2019-01-18 21:19:39 +0100

    hello icomoon og morph-icon

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PostComponent } from './post/post';
+import { MorphIconComponent } from './morph-icon/morph-icon';
 @NgModule({
-	declarations: [PostComponent],
+	declarations: [PostComponent,
+    MorphIconComponent],
 	imports: [],
-	exports: [PostComponent]
+	exports: [PostComponent,
+    MorphIconComponent]
 })
 export class ComponentsModule {}

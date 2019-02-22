d3daba92458070813c8e3c1bf449fef36bcda5bc
Marius Mandal <mariusmandal@gmail.com>
2019-02-22 23:37:53 +0100

    Bruk postspage

--- src/app/pages/info/info.module.ts
+++ src/app/pages/info/info.module.ts
@@ -5,25 +5,25 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { InfoPage } from './info.page';
 import { CommonComponentsModule } from 'src/app/modules/common-components/common-components.module';
-import { SinglePage } from './single.page';
+import { PostPage } from '../post/post.page';
+import { PostPageModule } from '../post/post.module';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-	RouterModule.forChild(
-		[
-			{ path: '', component: InfoPage },
-			{ path:'single/:id', component: SinglePage}
-		]
-	),
-	CommonComponentsModule
-  ],
-  declarations: [InfoPage, SinglePage]
+	imports: [
+		IonicModule,
+		CommonModule,
+		FormsModule,
+		PostPageModule,
+		RouterModule.forChild(
+			[
+				{ path: '', component: InfoPage },
+				{ path: 'single/:id', component: PostPage }
+			]
+		),
+		CommonComponentsModule
+	],
+	declarations: [
+		InfoPage
+	]
 })
-export class InfoPageModule {}
-
-
-
-//https://angularfirebase.com/snippets/how-manage-shared-components-in-an-ionic-4-app/
+export class InfoPageModule { }

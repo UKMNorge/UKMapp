4e676875ecb514855dddf170592b0ec32e527375
Marius Mandal <mariusmandal@gmail.com>
2019-02-22 23:38:03 +0100

    Homepage

--- src/app/pages/hjem/hjem.module.ts
+++ src/app/pages/hjem/hjem.module.ts
@@ -4,14 +4,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HjemPage } from './hjem.page';
+import { CommonComponentsModule } from 'src/app/modules/common-components/common-components.module';
+import { InnslagPage } from '../program/innslag.page';
+import { ProgramPageModule } from '../program/program.module';
+import { PostPage } from '../post/post.page';
+import { PostPageModule } from '../post/post.module';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    RouterModule.forChild([{ path: '', component: HjemPage }])
-  ],
-  declarations: [HjemPage]
+	imports: [
+		IonicModule,
+		CommonModule,
+		FormsModule,
+		ProgramPageModule,
+		PostPageModule,
+		RouterModule.forChild(
+			[
+				{ path: '', component: HjemPage },
+				{ path: 'nyhet/:post_id', component: PostPage },
+				{ path: 'innslag/:innslag', component: InnslagPage}
+			]
+		),
+		CommonComponentsModule
+	],
+	declarations: [
+		HjemPage
+	]
 })
-export class HjemPageModule {}
+export class HjemPageModule { }
--- src/app/pages/hjem/hjem.page.ts
+++ src/app/pages/hjem/hjem.page.ts
@@ -1,8 +1,74 @@
 import { Component } from '@angular/core';
+import { StreamingMedia, StreamingVideoOptions } from '@ionic-native/streaming-media/ngx';
+import { MonstringService } from 'src/app/services/ukmnorge/app/monstring.service';
+import { Innslag } from 'src/app/services/ukmnorge/api/innslag.models';
+import { UKMTV } from 'src/app/services/ukmnorge/api/filmer.models';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-page-hjem',
   templateUrl: 'hjem.page.html',
-  styleUrls: ['hjem.page.scss']
+  styleUrls: []//'hjem.page.scss']
 })
-export class HjemPage {}
+export class HjemPage {
+
+	public nyheter = null;
+	public alle_innslag = null;
+	public filmer = null;
+
+	constructor(
+		private streamingMedia: StreamingMedia,
+		private monstringService: MonstringService,
+		private navCtrl: NavController
+	) {		
+	}
+
+	ngOnInit() {
+		let self = this;
+		
+		// Last inn nyheter
+		this.monstringService.getPosts().getFrontlist().subscribe(
+			(nyheter) => {
+				console.log('GOT NEWS');
+				console.log( nyheter );
+				self.nyheter = nyheter;
+			}
+		);
+
+		// Last inn innslag
+		this.monstringService.getInnslag().getMonstringInnslag().subscribe(
+			(alle_innslag: Innslag[]) => {
+				self.alle_innslag = alle_innslag;
+			}
+		);
+
+		// Last inn siste filmer
+		this.monstringService.getFilmer().getMonstringSiste().subscribe(
+			(filmer: UKMTV[]) => {
+				self.filmer = filmer;
+			}
+		);
+	}
+
+
+	spillVideo(url) {
+		let options: StreamingVideoOptions = {
+			orientation: 'landscape',
+		  };
+		this.streamingMedia.playVideo(url, options);
+	}
+
+	visNyhet( id ) {
+		console.log('VIS NYHET'+ id );
+		this.navCtrl.navigateForward(
+			'app/app/hjem/nyhet/'+ id
+		);
+	}
+
+	visInnslag( id ) {
+		console.log('VIS INNSLAG'+ id );
+		this.navCtrl.navigateForward(
+			'app/app/hjem/innslag/'+ id
+		);
+	}
+}

ad684607e948a2ca787344027b8a0a3d2c1fa04d
Marius Mandal <mariusmandal@gmail.com>
2019-02-26 11:48:22 +0100

    Vis nettverk-status

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,8 +1,38 @@
 import { Component } from '@angular/core';
+import { NetworkService } from '../services/ukmnorge/utils/network.service';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {}
+export class TabsPage {
+
+	public infoWarning = false;
+
+	constructor(
+		private networkService: NetworkService
+	) {
+		let self = this;
+
+		networkService.change().subscribe(
+			( status ) => {
+				if( status ) {
+					self.showWarning();
+				} else {
+					self.hideWarning();
+				}
+			}
+		);
+	}
+
+
+	public showWarning() {
+		console.log('INFO SHOW LABEL WARNING');
+		this.infoWarning = true;
+	}
+	public hideWarning() {
+		console.log('INFO HIDE LABEL WARNING');
+		this.infoWarning = false;
+	}
+}

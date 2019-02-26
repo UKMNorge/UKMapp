a46494a99eca23f294e96e4fd6bd03b5df910ed3
Marius Mandal <mariusmandal@gmail.com>
2019-02-26 13:39:34 +0100

    importer CommonComponentsModule

--- src/app/pages/select/select.module.ts
+++ src/app/pages/select/select.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { SelectPage } from './select.page';
+import { CommonComponentsModule } from 'src/app/modules/common-components/common-components.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: SelectPage }])
+	RouterModule.forChild([{ path: '', component: SelectPage }]),
+	CommonComponentsModule
   ],
   declarations: [SelectPage]
 })
